114249b9b5509b87c8b4d417723db738ac5977b3
sannimdev <inegg.apps@gmail.com>
2022-02-04 18:50:02 +0900

    8.3 Router Setup part One

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route path='/banana/is'>
+                        <h1>Banana Is</h1>
+                    </Route>
+                    <Route path='/banana'>
+                        <h1>Banana</h1>
+                    </Route>
+                    <Route path='/potato'>
+                        <h1>Potato</h1>
+                    </Route>
+                    <Route path='/' exact>
+                        <h1>Home</h1>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
