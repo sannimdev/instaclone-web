cd37477b9c421de8469f8938c46e042eb60c83bb
sannimdev <inegg.apps@gmail.com>
2022-02-07 00:14:18 +0900

    10.1 Login UI Clone
    - 영상에서는 타임랩스를 진행하였지만, 진도를 빠르게 나가기 위해 나머지 코드는 nomadcoders에서 복붙하여 사용하기

--- README.md
+++ README.md
@@ -9,8 +9,7 @@
 ## 8.1 설치 라이브러리
 
 ```
-npm i styled-components react-hook-form react-router @apollo/client graphql reac
--helmet-async
+npm i styled-components react-hook-form react-router-dom @apollo/client graphql react-helmet-async
 npm i --save @fortawesome/fontawesome-svg-core
 npm install --save @fortawesome/free-solid-svg-icons
 npm install --save @fortawesome/react-fontawesome
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,20 +1,59 @@
-import styled from 'styled-components';
-import { darkModeVar } from '../apollo';
+import styled from "styled-components";
+import { darkModeVar } from "../apollo";
 
 const Title = styled.h1`
     color: ${(props) => props.theme.fontColor};
     font-size: 3rem;
 `;
 const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor};
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+const WhiteBox = styled.div`
+    background-color: white;
+    border-color: rgb(219, 219, 219);
+`;
+
+const TopBox = styled(WhiteBox)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    form {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+`;
+
+const BottomBox = styled(WhiteBox)`
+    padding: 10px 0;
+    text-align: center;
 `;
 
 const Login = () => {
     return (
         <Container>
-            <Title>Login</Title>
-            <button onClick={() => darkModeVar(true)}>To dark</button>
-            <button onClick={() => darkModeVar(false)}>To Light</button>
+            <div>
+                <TopBox>
+                    <h1>Instagram</h1>
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <input type="submit" value="Log in" />
+                    </form>
+                    <span>Or</span>
+                    <span>Log in with Facebook</span>
+                </TopBox>
+                <BottomBox>
+                    <span>Don't have an account?</span> <a href="#">Sign up</a>
+                </BottomBox>
+            </div>
         </Container>
     );
 };
