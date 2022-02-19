82dc995e55742596ba2c70d9eee4c176d6d81dc0
sannimdev <inegg.apps@gmail.com>
2022-02-19 22:09:01 +0900

    11.7 Liking Photos

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -16,7 +16,6 @@ function useUser() {
     const { data } = useQuery(ME_QUERY, {
         skip: !hasToken,
     });
-    console.log(data);
     useEffect(() => {
         if (data?.me === null) {
             logUserOut();
