5282f36bea96936e600f2c6a8f46e413392cbbc5
sannimdev <inegg.apps@gmail.com>
2022-02-19 22:48:51 +0900

    11.12 Comments part Two

--- src/components/feed/Photo.js
+++ src/components/feed/Photo.js
@@ -7,6 +7,7 @@ import styled from 'styled-components';
 import Avatar from '../../components/Avatar';
 import { FatText } from '../../components/shared';
 import { gql, useMutation } from '@apollo/client';
+import Comments from './Comments';
 
 const PhotoContainer = styled.div`
     background-color: white;
@@ -65,22 +66,6 @@ const TOGGLE_LIKE_MUTATION = gql`
             error
         }
     }
-`;
-
-const Comments = styled.div`
-    margin-top: 20px;
-`;
-const Comment = styled.div``;
-const CommentCaption = styled.span`
-    margin-left: 10px;
-`;
-
-const CommentCount = styled.span`
-    opacity: 0.7;
-    margin: 10px 0px;
-    display: block;
-    font-weight: 600;
-    font-size: 10px;
 `;
 
 function Photo({ id, user, file, isLiked, likes, caption, commentNumber, comments }) {
@@ -152,13 +137,7 @@ function Photo({ id, user, file, isLiked, likes, caption, commentNumber, comment
                     </div>
                 </PhotoActions>
                 <Likes>{likes === 1 ? '1 like' : `${likes} likes`}</Likes>
-                <Comments>
-                    <Comment>
-                        <FatText>{user.username}</FatText>
-                        <CommentCaption>{caption}</CommentCaption>
-                    </Comment>
-                    <CommentCount>{commentNumber === 1 ? '1 comment' : `${commentNumber} comments`}</CommentCount>
-                </Comments>
+                <Comments author={user.username} caption={caption} commentNumber={commentNumber} comments={comments} />
             </PhotoData>
         </PhotoContainer>
     );
@@ -175,7 +154,6 @@ Photo.propTypes = {
     isLiked: PropTypes.bool.isRequired,
     likes: PropTypes.number.isRequired,
     commentNumber: PropTypes.number.isRequired,
-    comments: PropTypes.arrayOf(PropTypes.shape({})),
 };
 
 export default Photo;
