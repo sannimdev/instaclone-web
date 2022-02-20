bc1aa9a5df1fec22aac3db6528d13a40e2bb4a2e
sannimdev <inegg.apps@gmail.com>
2022-02-20 14:20:47 +0900

    11.14 Parsing Hashtags part Two

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -2,6 +2,8 @@ import sanitizeHtml from 'sanitize-html';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { FatText } from '../shared';
+import { Link } from 'react-router-dom';
+import React from 'react';
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
@@ -23,7 +25,17 @@ function Comment({ author, payload = '' }) {
     return (
         <CommentContainer>
             <FatText>{author}</FatText>
-            <CommentCaption dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
+            <CommentCaption>
+                {payload.split(' ').map((word, idx) =>
+                    /#[\w]+/.test(word) ? (
+                        <React.Fragment key={idx}>
+                            <Link to={`/hashtags/${word}`}>{word}</Link>
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment key={idx}>{word}</React.Fragment>
+                    )
+                )}
+            </CommentCaption>
         </CommentContainer>
     );
 }
